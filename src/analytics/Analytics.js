908922ffed44d182c84d01040e01ad828f12d0ea
Brayan Ortega <brayansteven0322@gmail.com>
2024-06-06 01:44:51 -0500

    Add tables to analize information

--- src/analytics/Analytics.js
+++ src/analytics/Analytics.js
@@ -14,6 +14,8 @@ import { useState, useEffect } from 'react';
 import { Card, CardContent, Typography } from '@mui/material';
 import EmployeeTable from './EmployeeTable';
 import TopTypeDestinationsTable from './TopTypeDestinationsTable';
+import PopularDestinationsTable from './PopularDestinationsTable';
+
 
 
 
@@ -23,6 +25,8 @@ export default function Analytics() {
 
   const [planInfo, setPlanInfo] = useState([]);
   const [destinations, setDestinations] = useState([]);
+  const [types, setTypes] = useState([]);
+
 
   useEffect(() => {
     const fetchPlans = async () => {
@@ -34,7 +38,7 @@ export default function Analytics() {
       }
     };
 
-    const fetchTypes = async () => {
+    const fetchDestinations = async () => {
       try {
           const destinationsData = await PlanService.getTopDestinations();
           setDestinations(destinationsData);
@@ -44,6 +48,25 @@ export default function Analytics() {
           console.log(error);
       }
   };
+
+  const fetchTypes = async () => {
+    try {
+        const typesData = await PlanService.getTopTypes();
+        
+         const sortedTypes = typesData.sort((a, b) => b.number - a.number);
+        
+         const topThreeTypes = sortedTypes.slice();
+         
+         setTypes(topThreeTypes);
+
+         console.log(typesData);
+
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+fetchDestinations();
 
   fetchTypes();
 
@@ -59,33 +82,39 @@ export default function Analytics() {
   return (
     <div className='container'>
       <link href='https://fonts.googleapis.com/css?family=Rubik' rel='stylesheet'></link>
-      <TopBar>
+      <TopBar style = {{zIndex: 1000}}>
 
       </TopBar>
       <SideBar />
 
-      <div className='content'>
+      <div className='content' >
 
-        <div style={{ width: '60%',zIndex: '2', paddingLeft: '2px', alignItems: 'start', justifyContent: 'flex-start', display: 'flex' }}>
+        <div style={{ width: '60%',zIndex: '1', paddingLeft: '2px', alignItems: 'start', justifyContent: 'flex-start', display: 'flex' }}>
           <h2 className='tittle-plan'>Reportes</h2>
         </div>
 
-        <div style={{width: '60%'}}> 
-        <EmployeeTable data={planInfo} />
+        <div style={{width: '60%', zIndex: '1'}}> 
+        <EmployeeTable data={planInfo} style = {{zIndex:'2'}} />
         </div>
 
 
         
-        <div style={{ width: '60%',zIndex: '2', paddingLeft: '2px', alignItems: 'start', justifyContent: 'flex-start', display: 'flex' }}>
+        <div style={{ width: '60%',zIndex: '1', paddingLeft: '2px', alignItems: 'start', justifyContent: 'flex-start', display: 'flex' }}>
+          <h2 className='tittle-plan'>Destinos populares</h2>
+        </div>
+
+
+        <div style={{width: '40%', zIndex: '1'}}> 
+        <PopularDestinationsTable data={destinations} />
+        </div>
+
+        <div style={{ width: '60%',zIndex: '1', paddingLeft: '2px', alignItems: 'start', justifyContent: 'flex-start', display: 'flex' }}>
           <h2 className='tittle-plan'>Tipos de destino populares</h2>
         </div>
 
-
-        <div style={{width: '60%'}}> 
-        <TopTypeDestinationsTable data={destinations} />
+        <div style={{width: '40%', zIndex: '1'}}> 
+        <TopTypeDestinationsTable data={types} />
         </div>
-
-        
         
 
       </div>
--- src/analytics/PopularDestinationsTable.js
+++ src/analytics/PopularDestinationsTable.js
@@ -5,7 +5,7 @@ import { tableCellClasses } from '@mui/material/TableCell';
 import * as XLSX from 'xlsx';
 import { saveAs } from 'file-saver';
 
-const TopTypeDestinationsTable = ({data}) => {
+const PopularDestinationsTable = ({data}) => {
 
     const StyledTableCell = styled(TableCell)(({ theme }) => ({
         [`&.${tableCellClasses.head}`]: {
@@ -23,23 +23,22 @@ const TopTypeDestinationsTable = ({data}) => {
     const exportToExcel = () => {
       const exportData = data.map(content => ({
           "Nombre del destino": content.name,
-          "Cantidad de usos": content.number
-         
+          "Descripción": content.description
       }));
 
       const worksheet = XLSX.utils.json_to_sheet(exportData);
       const workbook = XLSX.utils.book_new();
-      XLSX.utils.book_append_sheet(workbook, worksheet, 'Tipos de destino populares');
+      XLSX.utils.book_append_sheet(workbook, worksheet, 'Destinos populares');
       const excelBuffer = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
       const dataBlob = new Blob([excelBuffer], { type: 'application/octet-stream' });
-      saveAs(dataBlob, 'Tipos_de_destino_populares.xlsx');
+      saveAs(dataBlob, 'Destinos_populares.xlsx');
     };
    
     return(
       <div style={{ position: 'relative', zIndex:'2 ' }}>
             <div style={{ position: 'sticky', top: 0, zIndex: 1000, width: '100%', backgroundColor: '#55B8A1', padding: '16px', display: 'flex', alignItems: 'center' }}>
                 <Typography variant="h6" component="div" style={{ color: 'white', margin: '0 auto' }}>
-                    Tipos de destino populares
+                    Destinos populares
                 </Typography>
                 <button onClick={exportToExcel} variant="contained" color="primary" style={{ left: '0', fontSize: '16px', width: 'auto', marginTop: '0px' }}>
                     Exportar a Excel
@@ -51,7 +50,7 @@ const TopTypeDestinationsTable = ({data}) => {
                   <TableRow>
 
                       <StyledTableCell>Nombre del Destino</StyledTableCell>
-                      <StyledTableCell>Cantidad de usos</StyledTableCell>
+                      <StyledTableCell>Descripción</StyledTableCell>
 
                   </TableRow>
               </TableHead>
@@ -60,7 +59,7 @@ const TopTypeDestinationsTable = ({data}) => {
                       <TableRow key={content.code}>
                           
                           <StyledTableCell>{content.name}</StyledTableCell>
-                          <StyledTableCell>{content.number}</StyledTableCell>
+                          <StyledTableCell>{content.description}</StyledTableCell>
                           
                       </TableRow>
                   ))}
@@ -72,4 +71,4 @@ const TopTypeDestinationsTable = ({data}) => {
 }
 
 
-export default TopTypeDestinationsTable;
+export default PopularDestinationsTable;
